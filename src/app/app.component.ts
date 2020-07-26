5317c87b5235b5560625458e19e1c7c31f7ee7a3
Dare Lawal <lawaldare@gmail.com>
2020-07-26 12:39:45 +0100

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import * as AOS from 'aos';
+import { slideInAnimation } from './app-animation';
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [slideInAnimation]
 })
 export class AppComponent implements OnInit {
   title = 'portfolio';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { ModalModule } from "ngx-bootstrap/modal";
 
@@ -28,7 +30,8 @@ import { SafePipe } from './@pipe/safe.pipe';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ModalModule.forRoot()
+    ModalModule.forRoot(),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
