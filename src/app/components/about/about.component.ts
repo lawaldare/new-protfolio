a709a7199875cb75b46522cb3ca57bb2b0669cb3
Dare Lawal <lawaldare@gmail.com>
2020-08-23 11:51:38 +0100

    updates

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { gsap, Power2, Elastic } from 'gsap/all';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
 
+
+export class AboutComponent implements OnInit {
 
   @ViewChild('renmoney', { static: true }) renmoney: ElementRef<HTMLDivElement>;
   @ViewChild('nxt', { static: true }) nxt: ElementRef<HTMLDivElement>;
@@ -18,10 +21,10 @@ export class AboutComponent implements OnInit {
   @ViewChild('workExperience', { static: true }) workExperience: ElementRef<HTMLHtmlElement>;
   @ViewChild('mainHeader', { static: true }) mainHeader: ElementRef<HTMLHtmlElement>;
   @ViewChild('subHeader', { static: true }) subHeader: ElementRef<HTMLHtmlElement>;
-
-
-
-
+  @ViewChild('contact', { static: true }) contact: ElementRef<HTMLDivElement>;
+  @ViewChild('icons', { static: true }) icons: ElementRef<HTMLDivElement>;
+  @ViewChild('devIcons', { static: true }) devIcons: ElementRef<HTMLDivElement>;
+  @ViewChild('contactHeader', { static: true }) contactHeader: ElementRef<HTMLHtmlElement>;
 
 
   constructor() { }
@@ -46,9 +49,12 @@ export class AboutComponent implements OnInit {
 
     header.from(this.mainHeader.nativeElement, { scale: 2 })
       .from(this.subHeader.nativeElement, { y: -500, duration: 3 })
-      .from('svg', { scale: 1, stagger: 0.5, transformOrigin: '50% 50%' }, '-=0.5')
+      .from(this.devIcons.nativeElement.childNodes, { scale: 1, stagger: 0.2, transformOrigin: '50% 50%' }, '-=0.5')
 
 
+    const contactAnimation = gsap.timeline({ defaults: { opacity: 0, ease: 'back', duration: 1 }, scrollTrigger: { trigger: this.contact.nativeElement, toggleActions: 'restart' } });
+
+    contactAnimation.from(this.contactHeader.nativeElement, { xPercent: -100, duration: 2 }).from(this.icons.nativeElement.childNodes, { stagger: 0.1, y: 100 });
 
   }
 
