72a2b27c4760f2ed53a8e51ac44a94b6f3bb05c1
dare lawal <darelawal@192.168.1.104>
2023-07-27 07:46:14 +0100

    updated the resume

--- src/app/components/navbar/navbar.component.spec.ts
+++ src/app/components/navbar/navbar.component.spec.ts
@@ -1,17 +1,18 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavbarComponent } from './navbar.component';
+import { SafePipe } from 'src/app/@pipe/safe.pipe';
+import { By } from '@angular/platform-browser';
 
-describe('NavbarComponent', () => {
+fdescribe('NavbarComponent', () => {
   let component: NavbarComponent;
   let fixture: ComponentFixture<NavbarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NavbarComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [NavbarComponent, SafePipe],
+    }).compileComponents();
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NavbarComponent);
@@ -21,5 +22,21 @@ describe('NavbarComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load all the links provided', () => {
+    const links = fixture.debugElement.queryAll(By.css('a'));
+    expect(links.length).toBe(4);
+  });
+
+  it('should call `setTitle` method when any link is clicked', () => {
+    const link = fixture.debugElement.queryAll(By.css('a'))[0];
+    spyOn(component, 'setTitle');
+
+    link.nativeElement.click();
+
+    expect(component.setTitle).toHaveBeenCalled();
+
+    // expect(links.length).toBe(4);
   });
 });
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,35 +1,15 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent implements OnInit {
-
-  // stickify: boolean;
-
-  constructor(private titleService: Title) { }
-
-
-  // @HostListener('window:scroll', ['$event'])
-  // onScroll(): void {
-  //   if (window.pageYOffset >= 120) {
-  //     this.stickify = true;
-  //   } else if (window.pageYOffset === 0) {
-  //     this.stickify = false;
-  //   }
-  // }
-
-  ngOnInit() {
-  }
-
-
-
+export class NavbarComponent {
+  constructor(private titleService: Title) {}
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
-
 }
