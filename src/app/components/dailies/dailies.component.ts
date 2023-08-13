90e3dd3d6893de77d3d43af0c7ca19b5479859f7
Dare Lawal <lawaldare@gmail.com>
2023-08-13 12:27:36 +0100

    update

--- src/app/components/dailies/dailies.component.ts
+++ src/app/components/dailies/dailies.component.ts
@@ -2,30 +2,26 @@ import { projectsDB } from './../../../db/dailies';
 import { DailyProject } from './../../models/dailyProject.model';
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-dailies',
   templateUrl: './dailies.component.html',
-  styleUrls: ['./dailies.component.scss']
+  styleUrls: ['./dailies.component.scss'],
 })
 export class DailiesComponent implements OnInit {
-
   link: string;
 
   projects: DailyProject[] = projectsDB;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   goToProjectWebsite(str) {
     window.open(str);
   }
 
   goToRepoLink(str) {
+    console.log(str);
     window.open(str);
   }
-
 }
--- src/db/dailies.ts
+++ src/db/dailies.ts
@@ -2,36 +2,46 @@ export const projectsDB = [
   {
     name: 'Loop 1 - Step 1',
     link: 'https://daily-projects.github.io/Loop-1---Step-1/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-1'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-1',
   },
   {
     name: 'Loop 1 - Step 2',
     link: 'https://daily-projects.github.io/Loop-1---Step-2/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-2'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-2',
   },
   {
     name: 'Loop 1 - Step 3',
     link: 'https://priceless-kare-72c3cc.netlify.app/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-3'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-3',
   },
   {
     name: 'Loop 1 - Step 4',
     link: 'https://daily-projects.github.io/Loop-1---Step-4/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-4'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-4',
   },
   {
     name: 'Loop 1 - Step 5',
     link: 'https://daily-projects.github.io/Loop-1---Step-5/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-5'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-5',
   },
   {
     name: 'Loop 1 - Step 6',
     link: 'https://daily-projects.github.io/Loop-1---Step-6/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-6'
+    repo: 'https://github.com/daily-projects/Loop-1---Step-6',
   },
   {
     name: 'Loop 1 - Step 7',
     link: 'https://daily-projects.github.io/Loop-1---Step-7/',
-    repo: 'https://github.com/daily-projects/Loop-1---Step-7'
-  }
-]
+    repo: 'https://github.com/daily-projects/Loop-1---Step-7',
+  },
+  {
+    name: 'Loop 1 - Step 8',
+    link: 'https://daily-projects.github.io/Loop-1---Step-8/',
+    repo: 'https://github.com/daily-projects/Loop-1---Step-8',
+  },
+  {
+    name: 'Loop 1 - Step 9',
+    link: 'https://daily-projects.github.io/Loop-1---Step-9/',
+    repo: 'https://github.com/daily-projects/Loop-1---Step-9',
+  },
+];
