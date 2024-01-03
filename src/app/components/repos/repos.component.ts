d50dfefe3ecdda5cace1e740579866c6347a2730
Dare Lawal <lawaldare@gmail.com>
2024-01-03 05:45:53 +0100

    update

--- src/app/components/repos/repos.component.ts
+++ src/app/components/repos/repos.component.ts
@@ -1,28 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Repo } from '../../models/repo.model';
-import { RepoComponent } from '../repo/repo.component';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit } from "@angular/core";
+import { Repo } from "../../models/repo.model";
+import { RepoComponent } from "../repo/repo.component";
+import { CommonModule } from "@angular/common";
 
 @Component({
-  selector: 'app-repos',
-  templateUrl: './repos.component.html',
-  styleUrls: ['./repos.component.scss'],
+  selector: "app-repos",
+  templateUrl: "./repos.component.html",
+  styleUrls: ["./repos.component.scss"],
   standalone: true,
   imports: [CommonModule, RepoComponent],
 })
 export class ReposComponent implements OnInit {
   repos: Repo[] = [
     {
-      link: 'https://github.com/lawaldare/Expense-Tracker-Widget-Angular-',
-      name: 'Expense Tracker (Angular Element)',
+      link: "https://github.com/lawaldare/Expense-Tracker-Widget-Angular-",
+      name: "Expense Tracker (Angular Element)",
     },
     {
-      link: 'https://github.com/lawaldare/bitcoin-calc-widget-angular',
-      name: 'Bitcoin Calculator (Angular Element)',
+      link: "https://github.com/lawaldare/bitcoin-calc-widget-angular",
+      name: "Bitcoin Calculator (Angular Element)",
     },
     {
-      link: 'https://github.com/lawaldare/stencil-weather-app',
-      name: 'Weather App (Stencil)',
+      link: "https://github.com/lawaldare/stencil-weather-app",
+      name: "Weather App (Stencil)",
+    },
+    {
+      link: "https://github.com/lawaldare/cc-widget",
+      name: "Credit Card (Angular Element)",
     },
   ];
 
