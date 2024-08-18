dec7cbb3c7bf442aa576f7a7fc8ecc9f40c85fbd
Dare Lawal <lawaldare@gmail.com>
2024-08-18 13:18:24 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import * as AOS from 'aos';
-import { slideInAnimation } from './app-animation';
-import { Router, NavigationEnd } from '@angular/router';
-import { filter } from 'rxjs/operators';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core";
+import * as AOS from "aos";
+import { Router, NavigationEnd } from "@angular/router";
+import { filter } from "rxjs/operators";
 
 declare var gtag;
 
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [slideInAnimation]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnInit {
-  title = 'portfolio';
+  title = "portfolio";
 
   constructor(router: Router) {
     const navEndEvents = router.events.pipe(
-      filter(event => event instanceof NavigationEnd),
+      filter((event) => event instanceof NavigationEnd)
     );
     navEndEvents.subscribe((event: NavigationEnd) => {
-      gtag('config', 'UA-162850535-1', {
-        'page_path': event.urlAfterRedirects
+      gtag("config", "UA-162850535-1", {
+        page_path: event.urlAfterRedirects,
       });
-    })
+    });
   }
 
   ngOnInit() {
     AOS.init();
   }
-
 
   // public setTitle(newTitle: string) {
   //   this.titleService.setTitle(newTitle);
--- src/app/components/repo/repo.component.ts
+++ src/app/components/repo/repo.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Repo } from 'src/app/models/repo.model';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from "@angular/core";
+import { Repo } from "src/app/models/repo.model";
 
 @Component({
-    selector: 'app-repo',
-    templateUrl: './repo.component.html',
-    styleUrls: ['./repo.component.scss'],
-    standalone: true,
+  selector: "app-repo",
+  templateUrl: "./repo.component.html",
+  styleUrls: ["./repo.component.scss"],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RepoComponent implements OnInit {
   @Input() repo: Repo;
